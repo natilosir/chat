c1f9ea5c112e6e6ea48872a74b5643ff5ff8a563
MMD <natilosnts@gmail.com>
2025-01-29 17:58:13 +0330

    Add files via upload

---
+++ database/migrations/2025_01_16_205120_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id'); // Define user_id as an unsigned integer
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); // Define foreign key
+            $table->string('username', 191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
